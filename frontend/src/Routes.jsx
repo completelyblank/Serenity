944b0080486d96381d78a21bd5a492fb94d3cc6c
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-09-07 07:34:00 +0500

    Update Routes.jsx

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,23 +2,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './App.jsx';
+import DreamAnalysisForm from './pages/log.jsx';
 import Home from './pages/home.jsx';
 //import ProfilePage from './pages/profile.jsx';
 // <Route path='/profile' element={<ProfilePage />} /> add this
 import Analysis from './pages/analysis.jsx';
-import Cloud_Journey from './pages/cloud_journey.jsx';
+import Jumper_Game from './pages/jumper_game.jsx';
 import Memory_Game from './pages/memory_game.jsx';
-import BreathingExercise from './pages/breather.jsx'
+import Quiz_Game from './pages/quiz_game.jsx'
 import MoodLoggingForm from './pages/log.jsx';
+//import ProfilePage from './pages/profile.jsx';
+import './index.css'
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<HomePage />} />
+      <Route path="/login" element={<Home />} />
       <Route path="/analysis" element={<Analysis />} />
       <Route path="/form" element={<MoodLoggingForm />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/journey" element={<Cloud_Journey />} />
-      <Route path="/breathe" element={<BreathingExercise />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/jumper" element={<Jumper_Game />} />
+      <Route path="/quiz" element={<Quiz_Game />} />
       <Route path="/memory" element={<Memory_Game />} />
       
     </Routes>
