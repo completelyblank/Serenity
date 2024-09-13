615688181c4e9b7fee03d39306754b254f93d44f
Amna Shah <75795497+amnashah110@users.noreply.github.com>
2024-09-13 21:36:34 +0500

    Add files via upload

---
+++ sql_scripts/users.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+  user_id NUMBER PRIMARY KEY, 
+  username VARCHAR2(30) NOT NULL UNIQUE, 
+  password VARCHAR2(30) NOT NULL, 
+  first_name VARCHAR2(20) NOT NULL, 
+  last_name VARCHAR2(20) NOT NULL, 
+  gender VARCHAR2(10) NOT NULL,
+  token_count NUMBER DEFAULT 0,
+  age NUMBER NOT NULL
+);
+
+CREATE SEQUENCE user_sequence 
+  START WITH 1 
+  INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER users_on_insert
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+  :new.user_id := user_sequence.NEXTVAL;
+END;
+/
+
+INSERT INTO users(username, password, first_name, last_name, gender, token_count, age) VALUES('zehra_waqar', 'helloworld', 'Zehra', 'Waqar', 'F', '2', '20');
+INSERT INTO users(username, password, first_name, last_name, gender, token_count, age) VALUES('amna_shah', 'acer2700', 'Amna', 'Shah', 'F', '12', '21');
+INSERT INTO users(username, password, first_name, last_name, gender, token_count, age) VALUES('muhammad_rk', 'simbuh', 'Muhammad', 'Raza Khan', 'M', '25', '22');
+
+SELECT * FROM users;
